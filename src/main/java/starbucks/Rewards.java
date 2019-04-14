9693b0ec7efb0bfba1a485c90b0f74104e613fb9
SudhaAmarnath <sudha04.a@gmail.com>
2019-04-13 19:27:54 -0700

    add constructor and display/touch methods

--- src/main/java/starbucks/Rewards.java
+++ src/main/java/starbucks/Rewards.java
@@ -5,10 +5,35 @@ package starbucks ;
 /** Rewards Screen */
 public class Rewards extends Screen
 {
+    private String rewardoptions;
 
+    /**
+     * Constructor
+     */
     public Rewards()
     {
 
     }
 
+    /**
+     * Displaying the rewards
+     * @return screen contents
+     */
+    @Override
+    public String display() {
+    	
+        rewardoptions = "Make Every" + "\n" + "Visit Count";
+        return rewardoptions;
+        
+    }
+
+    /**
+     * To handle touch events
+     * @param x Touch X Coord.
+     * @param y Touch Y Coord.
+     */
+    @Override
+    public void touch(int x, int y) {
+        // Do nothing
+    }
 }
